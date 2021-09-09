3c0ee667ffdcdfe710171ebca6a4fd9ab40eaa4b
Nora Junaid <njunaid@umass.edu>
2021-09-09 02:39:23 -0400

    Initial Commit

---
+++ You areAwesome!Tests/You_areAwesome_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  You_areAwesome_Tests.swift
+//  You areAwesome!Tests
+//
+//  Created by Nora Junaid on 9/9/21.
+//
+
+import XCTest
+@testable import You_areAwesome_
+
+class You_areAwesome_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
